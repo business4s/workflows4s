3757f38364d8b1f487eac60dcd01eaab32e5374e
hs094 <iamhardikat11@gmail.com>
2025-04-03 02:01:02 +0530

    Deprecate BPMNConverter in favor of BpmnRenderer for workflow rendering

--- workflows4s-bpmn/src/main/scala/workflows4s/bpmn/BPMNConverter.scala
+++ workflows4s-bpmn/src/main/scala/workflows4s/bpmn/BPMNConverter.scala
@@ -5,14 +5,12 @@ import workflows4s.wio.model.WIOModel
 
 object BPMNConverter {
 
-  /**
-   * @deprecated Use BpmnRenderer.renderWorkflow instead
-   */
+  /** @deprecated
+    *   Use BpmnRenderer.renderWorkflow instead
+    */
   @deprecated("Use BpmnRenderer.renderWorkflow instead", "")
   def convert(model: WIOModel, name: String): BpmnModelInstance = {
     BpmnRenderer.renderWorkflow(model, name)
   }
-
-
 
 }
