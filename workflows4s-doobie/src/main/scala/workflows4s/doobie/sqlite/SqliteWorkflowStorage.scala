aac6b6cec0b339243b76edaa121f291e13497445
Roman Greshny <greshny@gmail.com>
2025-06-10 20:40:44 +0200

    adapt storage

--- workflows4s-doobie/src/main/scala/workflows4s/doobie/sqlite/SqliteWorkflowStorage.scala
+++ workflows4s-doobie/src/main/scala/workflows4s/doobie/sqlite/SqliteWorkflowStorage.scala
@@ -1,18 +1,22 @@
 package workflows4s.doobie.sqlite
 
-import cats.effect.kernel.Resource
+import cats.effect.kernel.{Resource, Sync}
 import cats.syntax.all.*
 import doobie.*
 import doobie.implicits.*
-import workflows4s.doobie.WorkflowStorage
+import workflows4s.doobie.{ByteCodec, WorkflowStorage}
 
-class SqliteWorkflowStorage extends WorkflowStorage[String] {
-  override def getEvents(id: String): ConnectionIO[List[IArray[Byte]]] = {
-    sql"SELECT event_data FROM workflow_journal".query[Array[Byte]].map(IArray.unsafeFromArray).to[List]
-  }
+class SqliteWorkflowStorage[Event](tableName: String = "workflow_journal")(using eventCodec: ByteCodec[Event])
+    extends WorkflowStorage[String, Event] {
+  override def getEvents(id: String): fs2.Stream[ConnectionIO, Event] =
+    sql"SELECT event_data FROM workflow_journal"
+      .query[Array[Byte]]
+      .stream
+      .evalMap(bytes => Sync[ConnectionIO].fromTry(eventCodec.read(IArray.unsafeFromArray(bytes))))
 
-  override def saveEvent(id: String, event: IArray[Byte]): ConnectionIO[Unit] = {
-    sql"INSERT INTO workflow_journal (event_data) VALUES (${event.toArray})".update.run.void
+  override def saveEvent(id: String, event: Event): ConnectionIO[Unit] = {
+    val bytes = IArray.genericWrapArray(eventCodec.write(event)).toArray
+    sql"INSERT INTO workflow_journal (event_data) VALUES ($bytes)".update.run.void
   }
 
   override def lockWorkflow(id: String): Resource[ConnectionIO, Unit] = {
