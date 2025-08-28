f5f1a4875d3c51ad7b99b2680a5d09a7f53dd8c3
Watson Dinh <ping@w47s0n.com>
2025-08-28 06:45:22 -0500

    simplify example
    simplify

--- workflows4s-example/src/main/scala/workflows4s/example/docs/DraftSignalExample.scala
+++ workflows4s-example/src/main/scala/workflows4s/example/docs/DraftSignalExample.scala
@@ -2,20 +2,18 @@ package workflows4s.example.docs
 
 object DraftSignalExample {
 
-  // start_draft
   object DraftContext extends workflows4s.wio.WorkflowContext {
     // No need to define State or Event
   }
 
   import DraftContext._
 
+  // start_draft
   // Create a signal operation
   val awaitApproval = WIO.draft.signal("Approval Required", error = "Rejected")
 
   // Use it in a workflow
-  val workflow = WIO.draft.step("Submit PR") >>> 
-    awaitApproval >>> 
-    WIO.draft.step("Merge PR")
+  val workflow = WIO.draft.step("Submit PR") >>> awaitApproval
   // end_draft
 
 }
