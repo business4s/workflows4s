176098a517ea3e3b017d381d98eafe8a6a0a9e94
krrish175-byte <krrishbiswas175@gmail.com>
2026-01-10 23:27:32 +0530

    feat: POC for path-dependent WIO modularity

---
+++ workflows4s-core/src/main/scala/workflows4s/wio/experimental/PathDependentWIO.scala
@@ -0,0 +1,23 @@
+package workflows4s.wio.experimental
+
+trait Workflow {
+  type Event
+  type State
+
+  sealed trait WIO[-In, +Err, +Out] {
+    def flatMap[Err1 >: Err, Out1](f: Out => WIO[Out, Err1, Out1]): WIO[In, Err1, Out1] =
+      WIO.FlatMap(this, f)
+    
+    def map[Out1](f: Out => Out1): WIO[In, Err, Out1] =
+      this.flatMap(out => WIO.Pure(f(out)))
+  }
+
+  object WIO {
+    case class Pure[+Out](value: Out) extends WIO[Any, Nothing, Out]
+    
+    case class FlatMap[In, Err, Out, Out1](
+      base: WIO[In, Err, Out],
+      f: Out => WIO[Out, Err, Out1]
+    ) extends WIO[In, Err, Out1]
+  }
+}
---
+++ workflows4s-core/src/test/scala/workflows4s/wio/experimental/PathDependentWIOTest.scala
@@ -0,0 +1,24 @@
+package workflows4s.wio.experimental
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class PathDependentWIOTest extends AnyFreeSpec with Matchers {
+
+  "PathDependentWIO" - {
+    "should support basic types" in {
+       object MyWorkflow extends Workflow {
+         type Event = String
+         type State = Int
+       }
+
+       val step1: MyWorkflow.WIO[Any, Nothing, Int] = MyWorkflow.WIO.Pure(42)
+       val step2: MyWorkflow.WIO[Int, Nothing, String] = step1.map(_.toString)
+       println(step2)
+
+       // Just verifying that it compiles and types are correct
+       succeed
+    }
+  }
+
+}
