de8881db186d32d5890ccd5035b25e63308d244a
krrish175-byte <krrishbiswas175@gmail.com>
2026-01-17 16:44:54 +0530

    fix(doobie): skip Postgres tests when Docker is unavailable

--- workflows4s-doobie/src/test/scala/workflows4s/doobie/postgres/testing/PostgresSuite.scala
+++ workflows4s-doobie/src/test/scala/workflows4s/doobie/postgres/testing/PostgresSuite.scala
@@ -7,11 +7,34 @@ import com.dimafeng.testcontainers.scalatest.TestContainerForAll
 import doobie.*
 import doobie.implicits.*
 import doobie.util.transactor.Transactor
-import org.scalatest.Suite
+import org.scalatest.{BeforeAndAfterAll, Suite}
 
-trait PostgresSuite extends TestContainerForAll { self: Suite =>
+trait PostgresSuite extends TestContainerForAll with BeforeAndAfterAll { self: Suite =>
 
   override val containerDef: PostgreSQLContainer.Def = PostgreSQLContainer.Def()
+
+  override def beforeAll(): Unit = {
+    if isDockerAvailable() then {
+      super.beforeAll()
+    } else {
+      cancel("Docker is not available")
+    }
+  }
+
+  private def isDockerAvailable(): Boolean = {
+    try {
+      import scala.sys.process.*
+      import scala.concurrent.{Await, Future}
+      import scala.concurrent.duration.*
+      import scala.concurrent.ExecutionContext.Implicits.global
+      val future = Future {
+        Process("docker info").!(ProcessLogger(_ => ())) == 0
+      }
+      Await.result(future, 5.seconds)
+    } catch {
+      case _: Exception => false
+    }
+  }
 
   var xa: Transactor[IO] = scala.compiletime.uninitialized
 
