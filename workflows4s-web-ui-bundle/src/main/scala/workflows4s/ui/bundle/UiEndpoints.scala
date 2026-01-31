b7aecadf44425cc1f82e233e41a0fa46faf97bb0
Wojciech Pituła <wojciech@swissborg.com>
2026-01-31 17:48:42 +0100

    bring back generic tapir resource endpoint through path refix segregation

--- workflows4s-web-ui-bundle/src/main/scala/workflows4s/ui/bundle/UiEndpoints.scala
+++ workflows4s-web-ui-bundle/src/main/scala/workflows4s/ui/bundle/UiEndpoints.scala
@@ -1,16 +1,21 @@
 package workflows4s.ui.bundle
 
 import sttp.tapir.*
+import sttp.tapir.files.staticResourcesGetServerEndpoint
 import sttp.tapir.json.circe.jsonBody
 import sttp.tapir.server.ServerEndpoint
 import workflows4s.web.api.model.UIConfig
 
-
 object UiEndpoints {
+
+  // Reserved path segment for static assets - routes should never start with this
+  // vite automatically puts all assets in this folder
+  val AssetsPrefix = "assets"
 
   def get[F[_]](config: UIConfig): List[ServerEndpoint[Any, F]] = List(
     uiConfigEndpoint(config),
-    uiResourcesEndpoint,
+    uiAssetsEndpoint,
+    uiSpaEndpoint,
   )
 
   private def uiConfigEndpoint[F[_]](config: UIConfig) = endpoint.get
@@ -18,61 +23,31 @@ object UiEndpoints {
     .out(jsonBody[UIConfig])
     .serverLogicSuccessPure[F](_ => config)
 
-  private def uiResourcesEndpoint[F[_]] = 
+  // Serve static assets from /ui/assets/* using tapir's built-in static resources endpoint.
+  // This properly returns 404 for missing assets instead of falling back to index.html.
+  private def uiAssetsEndpoint[F[_]]: ServerEndpoint[Any, F] =
+    staticResourcesGetServerEndpoint[F](s"ui" / AssetsPrefix)(
+      this.getClass.getClassLoader,
+      s"workflows4s-web-ui-bundle/$AssetsPrefix",
+    )
+
+  // SPA catch-all: any route under /ui/* that doesn't match assets returns index.html.
+  // This enables client-side routing for the SPA.
+  private def uiSpaEndpoint[F[_]]: ServerEndpoint[Any, F] =
     endpoint.get
       .in("ui" / paths)
       .out(header[String]("Content-Type"))
       .out(inputStreamBody)
       .serverLogicPure[F] { pathList =>
-        val sanitizedPathList = pathList.filter(_ != ".")
-        val isValid = sanitizedPathList.forall { segment =>
-          segment.nonEmpty && 
-          segment != ".." && 
-          !segment.contains('/') && 
-          !segment.contains('\\') && 
-          !segment.contains(':')
-        }
-
-        if (!isValid) {
-          Left(())
-        } else {
-          val relativePath = sanitizedPathList.mkString("/")
-          if (relativePath.isEmpty) {
-            val indexStream = this.getClass.getClassLoader.getResourceAsStream("workflows4s-web-ui-bundle/index.html")
-            if (indexStream != null) Right(("text/html", indexStream))
-            else Left(())
-          } else {
-            val resourcePath = s"workflows4s-web-ui-bundle/$relativePath"
-            // try to find the resource
-            val stream = this.getClass.getClassLoader.getResourceAsStream(resourcePath)
-            if (stream != null) {
-              Right((guessContentType(relativePath), stream))
-            } else {
-              // fallback to index.html
-              val indexStream = this.getClass.getClassLoader.getResourceAsStream("workflows4s-web-ui-bundle/index.html")
-              if (indexStream != null) {
-                Right(("text/html", indexStream))
-              } else {
-                Left(())
-              }
-            }
-          }
-        }
+        // If someone requests /ui/assets/... and it got here, the asset doesn't exist
+        // (the assets endpoint didn't match). Return an error so we get a 404.
+        if pathList.headOption.contains(AssetsPrefix) then Left(())
+        else Right(indexHtmlResponse)
       }
-  private def guessContentType(path: String): String = {
-    val p = path.toLowerCase
-    if (p.endsWith(".html")) "text/html"
-    else if (p.endsWith(".js")) "application/javascript"
-    else if (p.endsWith(".css")) "text/css"
-    else if (p.endsWith(".json")) "application/json"
-    else if (p.endsWith(".png")) "image/png"
-    else if (p.endsWith(".svg")) "image/svg+xml"
-    else if (p.endsWith(".jpg") || p.endsWith(".jpeg")) "image/jpeg"
-    else if (p.endsWith(".gif")) "image/gif"
-    else if (p.endsWith(".ico")) "image/x-icon"
-    else if (p.endsWith(".woff2")) "font/woff2"
-    else if (p.endsWith(".woff")) "font/woff"
-    else if (p.endsWith(".ttf")) "font/ttf"
-    else "application/octet-stream"
+
+  private def indexHtmlResponse = {
+    val indexStream = this.getClass.getClassLoader.getResourceAsStream("workflows4s-web-ui-bundle/index.html")
+    assert(indexStream != null, "index.html not found in the classpath. Please check that workflows4s-web-ui-bundle is on the classpath.")
+    ("text/html", indexStream)
   }
 }
