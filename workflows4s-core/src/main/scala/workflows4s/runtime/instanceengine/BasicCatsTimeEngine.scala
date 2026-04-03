6d632bee1794149ba3999165b6553ac3753edbc3
Wojciech Pituła <wojciech@swissborg.com>
2026-04-03 16:09:31 +0200

    bring back applicative

--- workflows4s-core/src/main/scala/workflows4s/runtime/instanceengine/BasicCatsTimeEngine.scala
+++ workflows4s-core/src/main/scala/workflows4s/runtime/instanceengine/BasicCatsTimeEngine.scala
@@ -1,12 +1,12 @@
 package workflows4s.runtime.instanceengine
 
-import cats.Functor
+import cats.Applicative
 import cats.effect.Clock
 import cats.syntax.functor.*
 
 import java.time.Instant
 
-class BasicCatsTimeEngine[F[_]: Functor](clock: Clock[F]) extends BasicEngine[F] {
+class BasicCatsTimeEngine[F[_]: Applicative](clock: Clock[F]) extends BasicEngine[F] {
 
   override protected def now: F[Instant] = clock.realTime.map(x => Instant.ofEpochMilli(x.toMillis))
 
