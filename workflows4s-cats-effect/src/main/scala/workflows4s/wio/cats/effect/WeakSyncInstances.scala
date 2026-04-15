4f8f00ca324be444fc6447d78b43d5148964ec67
Wojciech Pituła <wojciech@swissborg.com>
2026-04-15 11:29:41 +0200

    more weaksync instances

--- workflows4s-cats-effect/src/main/scala/workflows4s/wio/cats/effect/WeakSyncInstances.scala
+++ workflows4s-cats-effect/src/main/scala/workflows4s/wio/cats/effect/WeakSyncInstances.scala
@@ -1,10 +1,10 @@
 package workflows4s.wio.cats.effect
 
-import _root_.cats.effect.IO
+import _root_.cats.effect.Sync
 import workflows4s.wio.WeakSync
 
 object WeakSyncInstances {
-  given WeakSync[IO] with {
-    override def delay[A](body: => A): IO[A] = IO(body)
+  given [F[_]](using F: Sync[F]): WeakSync[F] with {
+    override def delay[A](body: => A): F[A] = F.delay(body)
   }
 }
